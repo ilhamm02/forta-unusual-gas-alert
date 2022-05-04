b855c9d73192fdf666af935aaea5dd11abad69db
ilhamm02 <ilhamaulana24@gmail.com>
2022-05-04 15:35:10 +0700

    add metadate and replace some message

--- src/agent.py
+++ src/agent.py
@@ -11,11 +11,18 @@ def handle_transaction(transaction_event):
         gas_now = transaction_event.gas_price/10**9
         if gas_now > last_block_highest+(last_block_highest*50/100):
             findings.append(Finding({
-                'name': "Tremendous increase in gas costs",
-                'description': f'The increase in gas costs is {(gas_now-last_block_highest)/last_block_highest*100}% greater than the previous block. Last block highest gas price is {last_block_highest} GWEI, gas price this tx is {gas_now} GWEI',
+                'name': "Tremendous increase in gas price",
+                'description': f'The increase in gas price is {(gas_now-last_block_highest)/last_block_highest*100:.2f}% greater than the previous block.',
                 'alert_id': "GAS-DETECT-TCD",
                 'severity': FindingSeverity.Info,
                 'type': FindingType.Suspicious,
+                'metadata': {
+                    'block_number': transaction_event.block_number,
+                    'tx_hash': transaction_event.hash,
+                    'highest_gas_previous_block': last_block_highest,
+                    'gas_price': gas_now,
+                    'percentage': f'{(gas_now-last_block_highest)/last_block_highest:.2f}'
+                }
             }))
             
     if transaction_event.block.number > LAST_BLOCK[0]:
